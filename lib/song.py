d3c34154349ea3f16e541c6201218f8434694331
ldv432 <ldv432@gmail.com>
2024-10-30 16:31:05 -0600

    Song app lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,38 @@
+song_genres = ["Rap", "Pop", "Rock"]
+
 class Song:
-    pass
+    count=0
+    genres=[]
+    artists = [] 
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        type(self).add_song_to_count()
+        type(self).add_to_genres(genre)
+        type(self).add_to_artists(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        
+    @classmethod
+    def add_to_artists(cls, artist):
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+        if artist not in cls.genres:
+            cls.artists.append(artist)
+    
+    
+        
+    
+
+
